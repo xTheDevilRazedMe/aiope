9189cae63cd5195f093cf47c2da6a1f9b7fa6f75
xnet-admin-1 <xnet-admin-1@users.noreply.github.com>
2026-04-19 21:32:10 +0000

    Fix subagent context reference

--- feature-chat/src/main/kotlin/com/aiope2/feature/chat/ChatViewModel.kt
+++ feature-chat/src/main/kotlin/com/aiope2/feature/chat/ChatViewModel.kt
@@ -799,7 +799,7 @@ class ChatViewModel @Inject constructor(
     ).also { te ->
       _subagentManager = com.aiope2.feature.chat.engine.SubagentManager(
         createOrchestrator = { tools, onToolCall ->
-          val taskStore = com.aiope2.core.network.TaskModelStore(ctx)
+          val taskStore = com.aiope2.core.network.TaskModelStore(getApplication())
           val tc = taskStore.getTaskConfig(com.aiope2.core.network.ModelTask.SUBAGENT)
           val p = if (tc.profileId != null) providerStore.getById(tc.profileId!!) ?: providerStore.getActive() else providerStore.getActive()
           val modelId = tc.modelId ?: p.selectedModelId
