8e16be240cc9a41f10c1c45e4afc24f07e6fdcc4
Ubuntu <ubuntu@ip-10-0-1-6.us-west-2.compute.internal>
2026-06-18 19:08:56 +0000

    bump: v3.2.0

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -3,8 +3,8 @@ object Configurations {
   const val targetSdk = 34
   const val minSdk = 26
   const val majorVersion = 3
-  const val minorVersion = 1
+  const val minorVersion = 2
   const val patchVersion = 0
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 14
+  const val versionCode = 15
 }
